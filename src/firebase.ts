8f62542edb2eea0d48de955409f728da8ef76ac4
Manu <manojamanu512@gmail.com>
2026-05-21 18:21:48 +0530

    fixed firestore export

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -6,3 +9,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
