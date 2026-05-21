3a002915953da233e3e90a29f090fff2ed61ad07
Manu <manojamanu512@gmail.com>
2026-05-21 17:48:45 +0530

    merge conflict fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ export const Footer: React.FC = () => {
             <ul className="space-y-3.5 text-sm">
               <li className="flex items-center gap-3">
                 <Phone className="w-4 h-4 text-blue-500" />
-                <span className="text-slate-300 font-semibold">+91 91084 66084</span>
+                <span className="text-slate-300 font-semibold">+91 70198 86577</span>
               </li>
               <li className="flex items-center gap-3">
                 <Mail className="w-4 h-4 text-blue-500" />
